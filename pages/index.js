8427126a7ffc50bc22ae74f97a5f8d3e4d98e9be
Dinakaran <dinakaran@Dinakarans-MacBook-Air.local>
2021-01-23 00:56:51 +0530

    testing ci-cd

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to CI-CD enabled <a href="https://nextjs.org">New Next.js!</a>
+          Welcome to AWS DevOps CI-CD enabled <a href="https://nextjs.org">New Next.js!</a>
         </h1>
 
         <p className={styles.description}>
